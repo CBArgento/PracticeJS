c83208f336485de25717ce345e1a568e5c142b71
Lucas Di Fini <difinile@gmail.com>
2019-10-19 10:36:32 -0600

    adding img

---
+++ assets/javascript/script.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+    $(this).click(function() {
+        $("#h3id").css("color", "red");
+        $("#pid").css("color", "white");
+        $("#pid").css("background-color", "blue");
+    },
+    )
+    
+})
+
+;
+
+console.log();
